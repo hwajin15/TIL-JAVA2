ea27439698f4f012ea123dd3af924f46da9489b0
unknown <suhaw0325@naver.com>
2020-07-16 15:54:53 +0900

    day05

--- day05/myconnection~4.sql
+++ day05/myconnection~4.sql
@@ -9,3 +9,34 @@ select * from dept80;
 
 alter table dept80
 add(job_id varchar2(9));
+
+ALTER table dept80 
+modify(last_name varchar2(30));
+
+desc dept80;
+
+alter table dept80 
+drop( job_id);
+
+select * from dept80;
+
+alter table dept80 set unused(hire_date);
+select * from dept80;
+
+alter table dept80
+set unused(last_name);
+
+alter table dept80
+drop UNUSED COLUMNS;
+
+alter table copy_emp read only;
+
+select * from copy_emp;
+
+drop table copy_emp;
+
+drop table dept80;
+
+select * from dept80;
+
+flashback table dept80 to before drop;
